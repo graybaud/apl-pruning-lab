3efd65ad3bdefd29743a2594febd864bd593fc10
graybaud <raybaud.gabriel@gmail.com>
2026-06-07 13:35:39 +0200

    v0.3.1: LayerScorer, LayerCache, fused ops, 97 tests, 0 warnings

--- apl_pruning/__init__.py
+++ apl_pruning/__init__.py
@@ -1,5 +1,12 @@
 from .parser import MiniAPLParser
 from .exporter import to_pytorch, to_pytorch_function
+from .layers import LayerScorer
+from .cache import LayerCache, fused_abs_mean, fused_abs_max, fused_abs_sum, fused_norm_ratio
 
-__version__ = "0.3.0"
-__all__ = ["MiniAPLParser", "to_pytorch", "to_pytorch_function"]
+__version__ = "0.3.1"
+__all__ = [
+    "MiniAPLParser",
+    "to_pytorch", "to_pytorch_function",
+    "LayerScorer", "LayerCache",
+    "fused_abs_mean", "fused_abs_max", "fused_abs_sum", "fused_norm_ratio",
+]
