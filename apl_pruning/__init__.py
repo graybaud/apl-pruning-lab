afbc58ada88ed6463f065f06fef1d447e017a78c
graybaud <raybaud.gabriel@gmail.com>
2026-06-07 14:01:44 +0200

    v0.4.1: CastNet bridge, 16 APL pruning methods, 112 tests

--- apl_pruning/__init__.py
+++ apl_pruning/__init__.py
@@ -2,13 +2,13 @@ from .parser import MiniAPLParser
 from .exporter import to_pytorch, to_pytorch_function
 from .layers import LayerScorer
 from .cache import LayerCache, fused_abs_mean, fused_abs_max, fused_abs_sum, fused_norm_ratio
-from .scorers import METHODS, score_layer, get_formula, get_pytorch as get_method_pytorch, list_methods, compare_methods
+from .scorers import METHODS, score_layer, get_formula, get_pytorch, list_methods, compare_methods
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __all__ = [
     "MiniAPLParser",
     "to_pytorch", "to_pytorch_function",
     "LayerScorer", "LayerCache",
     "fused_abs_mean", "fused_abs_max", "fused_abs_sum", "fused_norm_ratio",
-    "METHODS", "score_layer", "get_formula", "get_method_pytorch", "list_methods", "compare_methods",
+    "METHODS", "score_layer", "get_formula", "get_pytorch", "list_methods", "compare_methods",
 ]
